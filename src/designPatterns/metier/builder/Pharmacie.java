27198b842276fa89871b7fb4264aa213bc0c46c2
Dorian <Dorian@Dorian-PC>
2019-05-02 17:07:44 +0200

    Commit 16 : design patterns builder v1

---
+++ src/designPatterns/metier/builder/Pharmacie.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designPatterns.metier.builder;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author dorian.blumart
+ */
+public class Pharmacie {
+
+    public static void main(String[] args) {
+        
+        try {
+            
+        } catch (Exception e) {
+            System.out.println("erreur " + e);
+        }
+    }
+}
---
+++ src/designPatterns/metier/builder/Prescription.java
@@ -0,0 +1,126 @@
+
+package designPatterns.metier.builder;
+
+import designPatterns.metier.Infos;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import pharmacie.metier.Medecin;
+import pharmacie.metier.Patient;
+
+
+/**
+ * classe métier de gestion d'un client
+ * @author Michel Poriaux
+ * @version 1.0
+ * 
+ */
+
+
+public class Prescription {
+    protected int idpres;
+    protected LocalDate dateprescription;
+    protected Set<Infos> info = new HashSet<>();
+    protected Patient patient;
+    protected Medecin medecin;
+
+
+   private Prescription(PrescriptionBuilder cb) {
+        this.idpres = cb.idpres;
+        this.dateprescription = cb.dateprescription;
+        this.info = cb.info;
+        this.patient = cb.patient;
+        this.medecin = cb.medecin;
+    }
+
+    public int getIdpres() {
+        return idpres;
+    }
+
+    public LocalDate getDateprescription() {
+        return dateprescription;
+    }
+
+    public Set<Infos> getInfo() {
+        return info;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public Medecin getMedecin() {
+        return medecin;
+    }
+
+    @Override
+    public String toString() {
+        return "Prescription{" + "idpres=" + idpres + ", dateprescription=" + dateprescription + ", info=" + info + ", patient=" + patient + ", medecin=" + medecin + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 89 * hash + this.idpres;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Prescription other = (Prescription) obj;
+        if (this.idpres != other.idpres) {
+            return false;
+        }
+        return true;
+    }
+
+    
+    public static class PrescriptionBuilder{
+    protected int idpres;
+    protected LocalDate dateprescription;
+    protected Set<Infos> info = new HashSet<>();
+    protected Patient patient;
+    protected Medecin medecin;
+
+        public PrescriptionBuilder setIdpres(int idpres) {
+            this.idpres = idpres;
+            return this;
+        }
+
+        public PrescriptionBuilder setDateprescription(LocalDate dateprescription) {
+            this.dateprescription = dateprescription;
+            return this;
+        }
+
+        public PrescriptionBuilder setInfo(Set<Infos> info) {
+            this.info = info;
+            return this;
+        }
+
+        public PrescriptionBuilder setPatient(Patient patient) {
+            this.patient = patient;
+            return this;
+        }
+
+        public PrescriptionBuilder setMedecin(Medecin medecin) {
+            this.medecin = medecin;
+            return this;
+        }
+     
+        public Prescription build() throws Exception{
+            if(idpres<=0 || patient==null || medecin==null || info==null) throw new Exception("informations de construction incomplètes");
+            return new Prescription(this);
+        }
+
+}
+    
+}
